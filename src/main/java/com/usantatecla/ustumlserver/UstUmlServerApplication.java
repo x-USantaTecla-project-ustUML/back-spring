0833229d19f83e48e3ce31aa164174c405ecdfb6
MRxRafi <rafadriveclase@gmail.com>
2021-04-26 13:33:44 +0200

    Initial commit

---
+++ src/main/java/com/usantatecla/ustumlserver/UstUmlServerApplication.java
@@ -0,0 +1,13 @@
+package com.usantatecla.ustumlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstUmlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstUmlServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usantatecla/ustumlserver/UstUmlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usantatecla.ustumlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UstUmlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
