a90871449090f98a660ecf49ba27e0079bdc5374
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-05-06 23:41:08 +0200

    add CrossOrigin

--- src/main/java/com/usantatecla/ustumlserver/api/resources/CommandResource.java
+++ src/main/java/com/usantatecla/ustumlserver/api/resources/CommandResource.java
@@ -6,13 +6,11 @@ import com.usantatecla.ustumlserver.domain.model.UstUMLGenerator;
 import com.usantatecla.ustumlserver.domain.services.Command;
 import com.usantatecla.ustumlserver.domain.services.PackageService;
 import org.json.JSONObject;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping(CommandResource.COMMAND)
 public class CommandResource {
