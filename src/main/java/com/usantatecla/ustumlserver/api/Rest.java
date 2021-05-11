fb4a93235099dcfd6f335d9559faef6d77f6a066
Pablo <pabloguijarro96@gmail.com>
2021-05-11 18:18:21 +0200

    CrossOrigin fixed

--- src/main/java/com/usantatecla/ustumlserver/api/Rest.java
+++ src/main/java/com/usantatecla/ustumlserver/api/Rest.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin("http://localhost:4200")
 @RestController
 public @interface Rest {
 }
