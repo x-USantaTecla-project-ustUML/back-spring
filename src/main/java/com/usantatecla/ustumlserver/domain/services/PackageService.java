ca3c479054f59dc2ad5f13c2b00520ff1d4de856
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2021-05-12 13:21:39 +0200

    data base configuration #8

--- src/main/java/com/usantatecla/ustumlserver/domain/services/PackageService.java
+++ src/main/java/com/usantatecla/ustumlserver/domain/services/PackageService.java
@@ -2,6 +2,8 @@ package com.usantatecla.ustumlserver.domain.services;
 
 import com.usantatecla.ustumlserver.domain.model.Member;
 import com.usantatecla.ustumlserver.domain.model.Package;
+import com.usantatecla.ustumlserver.domain.persistence.PackagePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,25 +11,31 @@ import java.util.ArrayList;
 @Service
 public class PackageService {
 
-    private Package pakage;
+    //private Package pakage;
+    private PackagePersistence packagePersistence;
 
-    public PackageService() {
-        this.pakage = new Package("name", new ArrayList<>());
+    @Autowired
+    public PackageService(PackagePersistence packagePersistence) {
+        //this.pakage = new Package("name", new ArrayList<>());
+        this.packagePersistence = packagePersistence;
     }
 
     public Package get(Command command) {
+        Package pakage = this.packagePersistence.read("name");
         CommandType commandType = command.getCommandType();
         for (Command memberCommand : command.getMembers()) {
             MemberType memberType = memberCommand.getMemberType();
             if (commandType == CommandType.ADD) {
                 Member member = memberType.create().add(memberCommand);
-                if (!this.pakage.find(member.getName())) {
-                    this.pakage.add(member);
+                if (!pakage.find(member.getName())) {
+                    pakage.add(member);
                 } else {
                     throw new CommandParserException(Error.MEMBER_ALREADY_EXISTS, member.getName());
                 }
             }
         }
-        return this.pakage;
+        this.packagePersistence.update(pakage);
+        return pakage;
     }
+
 }
