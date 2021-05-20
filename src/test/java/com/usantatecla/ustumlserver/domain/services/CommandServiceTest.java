12b9111506738e9b84a2ee5d8ab7ee9d7ed496bb
MRxRafi <rafadriveclase@gmail.com>
2021-05-20 17:57:30 +0200

    Unit tests for Open & close command and fixed other tests #17

--- src/test/java/com/usantatecla/ustumlserver/domain/services/CommandServiceTest.java
+++ src/test/java/com/usantatecla/ustumlserver/domain/services/CommandServiceTest.java
@@ -3,21 +3,25 @@ package com.usantatecla.ustumlserver.domain.services;
 import com.usantatecla.ustumlserver.TestConfig;
 import com.usantatecla.ustumlserver.domain.model.Package;
 import com.usantatecla.ustumlserver.domain.model.PackageBuilder;
-import com.usantatecla.ustumlserver.infrastructure.mongodb.daos.Seeder;
+import com.usantatecla.ustumlserver.domain.services.parsers.CommandParserException;
+import com.usantatecla.ustumlserver.infrastructure.mongodb.daos.TestSeeder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestConfig
 public class CommandServiceTest {
 
-   /* @Autowired
+    private static String SESSION_ID = "TEST";
+
+    @Autowired
     private CommandService commandService;
     @Autowired
-    private Seeder seeder;
+    private TestSeeder seeder;
 
     @BeforeEach
     void beforeEach() {
@@ -37,7 +41,7 @@ public class CommandServiceTest {
                 "   }" +
                 "}").build();
         Package expected = new PackageBuilder().clazz().name(name).build();
-        assertThat(this.commandService.execute(command), is(expected));
+        assertThat(this.commandService.execute(command, CommandServiceTest.SESSION_ID), is(expected));
     }
 
     @Test
@@ -49,7 +53,7 @@ public class CommandServiceTest {
                 "   }" +
                 "}").build();
         Package expected = new PackageBuilder().build();
-        assertThat(this.commandService.execute(command), is(expected));
+        assertThat(this.commandService.execute(command, CommandServiceTest.SESSION_ID), is(expected));
     }
 
     @Test
@@ -65,7 +69,7 @@ public class CommandServiceTest {
                 "   }" +
                 "}").build();
         Package expected = new PackageBuilder().clazz().name(firstName).clazz().name(secondName).build();
-        assertThat(this.commandService.execute(command), is(expected));
+        assertThat(this.commandService.execute(command, CommandServiceTest.SESSION_ID), is(expected));
     }
 
     @Test
@@ -83,7 +87,55 @@ public class CommandServiceTest {
         Package expected = new PackageBuilder()
                 .pakage(new PackageBuilder().name(name).build())
                 .build();
-        assertThat(this.commandService.execute(command), is(expected));
-    }*/
+        assertThat(this.commandService.execute(command, CommandServiceTest.SESSION_ID), is(expected));
+    }
+
+    @Test
+    void testGivenCommandServiceWhenExecuteOpenThenReturn() {
+        this.seeder.seedOpen();
+        String name = "test";
+        Command command = new CommandBuilder().command("{" +
+                "   open:" + name +
+                "}").build();
+        Package expected = new PackageBuilder().name(name).build();
+        assertThat(this.commandService.execute(command, TestSeeder.SESSION_OPEN), is(expected));
+    }
+
+    @Test
+    void testGivenCommandServiceWhenExecuteOpenThenThrowException() {
+        this.seeder.seedOpen();
+        Command command = new CommandBuilder().command("{" +
+                "   open: not_found" +
+                "}").build();
+        assertThrows(CommandParserException.class, () -> this.commandService.execute(command, TestSeeder.SESSION_OPEN));
+    }
+
+    @Test
+    void testGivenCommandServiceWhenExecuteOpenOnClassThenThrowException() {
+        this.seeder.seedOpen();
+        Command command = new CommandBuilder().command("{" +
+                "   open: TestClass" +
+                "}").build();
+        this.commandService.execute(command, TestSeeder.SESSION_OPEN);
+        assertThrows(CommandParserException.class, () -> this.commandService.execute(command, TestSeeder.SESSION_OPEN));
+    }
+
+    @Test
+    void testGivenCommandServiceWhenExecuteCloseThenReturn() {
+        this.seeder.seedClose();
+        Command command = new CommandBuilder().command("{" +
+                "   close: null" +
+                "}").build();
+        String expectedName = "name";
+        assertThat(this.commandService.execute(command, TestSeeder.SESSION_OPEN).getName(), is(expectedName));
+    }
+
+    @Test
+    void testGivenCommandServiceWhenExecuteCloseThenThrowException() {
+        Command command = new CommandBuilder().command("{" +
+                "   close: null" +
+                "}").build();
+        assertThrows(CommandParserException.class, () -> this.commandService.execute(command, TestSeeder.SESSION_OPEN));
+    }
 
 }
---
+++ src/test/java/com/usantatecla/ustumlserver/infrastructure/mongodb/daos/TestSeeder.java
@@ -0,0 +1,68 @@
+package com.usantatecla.ustumlserver.infrastructure.mongodb.daos;
+
+import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.ClassEntity;
+import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.MemberEntity;
+import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.PackageEntity;
+import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.SessionEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TestSeeder {
+
+    public static String SESSION_OPEN = "OPEN";
+
+    private Seeder seeder;
+    private PackageDao packageDao;
+    private ClassDao classDao;
+    private UserDao userDao;
+    private SessionDao sessionDao;
+
+    @Autowired
+    public TestSeeder(Seeder seeder, PackageDao packageDao, ClassDao classDao, UserDao userDao, SessionDao sessionDao) {
+        this.seeder = seeder;
+        this.packageDao = packageDao;
+        this.classDao = classDao;
+        this.userDao = userDao;
+        this.sessionDao = sessionDao;
+        this.initialize();
+    }
+
+    public void initialize() {
+        this.seeder.initialize();
+        this.deleteAll();
+    }
+
+    private void deleteAll() {
+        this.sessionDao.deleteAll();
+    }
+
+    public void seedOpen() {
+        ClassEntity classEntity = ClassEntity.builder().name("TestClass").build();
+        this.classDao.save(classEntity);
+        PackageEntity packageEntity = PackageEntity.builder()
+                .name("test").build();
+        this.packageDao.save(packageEntity);
+        PackageEntity mainPackage = this.packageDao.findByName("name");
+        List<MemberEntity> memberEntities = List.of(packageEntity, classEntity);
+        mainPackage.setMemberEntities(memberEntities);
+        this.packageDao.save(mainPackage);
+        SessionEntity openSession = SessionEntity.builder()
+                .sessionId(TestSeeder.SESSION_OPEN)
+                .memberEntities(List.of(mainPackage))
+                .build();
+        this.sessionDao.save(openSession);
+    }
+
+    public void seedClose() {
+        this.seedOpen();
+        SessionEntity sessionEntity = this.sessionDao.findBySessionId(TestSeeder.SESSION_OPEN);
+        List<MemberEntity> memberEntities = sessionEntity.getMemberEntities();
+        memberEntities.add(this.packageDao.findByName("test"));
+        sessionEntity.setMemberEntities(memberEntities);
+        this.sessionDao.save(sessionEntity);
+    }
+
+}
