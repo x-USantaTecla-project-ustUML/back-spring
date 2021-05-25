a63b29fa5bf0b264c59b1ef5a9a99406eb3956c3
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-05-25 13:32:02 +0200

    refactor persistence visitors #24

--- src/main/java/com/usantatecla/ustumlserver/infrastructure/mongodb/persistence/SessionPersistenceMongodb.java
+++ src/main/java/com/usantatecla/ustumlserver/infrastructure/mongodb/persistence/SessionPersistenceMongodb.java
@@ -2,6 +2,7 @@ package com.usantatecla.ustumlserver.infrastructure.mongodb.persistence;
 
 import com.usantatecla.ustumlserver.domain.model.Class;
 import com.usantatecla.ustumlserver.domain.model.Member;
+import com.usantatecla.ustumlserver.domain.model.MemberVisitor;
 import com.usantatecla.ustumlserver.domain.model.Package;
 import com.usantatecla.ustumlserver.domain.persistence.SessionPersistence;
 import com.usantatecla.ustumlserver.infrastructure.api.dtos.ErrorMessage;
@@ -12,6 +13,7 @@ import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.ClassEntity;
 import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.MemberEntity;
 import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.PackageEntity;
 import com.usantatecla.ustumlserver.infrastructure.mongodb.entities.SessionEntity;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -20,13 +22,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@Data
 @Repository
 public class SessionPersistenceMongodb implements SessionPersistence {
 
     private SessionDao sessionDao;
     private PackageDao packageDao;
     private ClassDao classDao;
-    private MemberEntity memberEntity;
 
     @Autowired
     public SessionPersistenceMongodb(SessionDao sessionDao, PackageDao packageDao, ClassDao classDao) {
@@ -54,8 +56,7 @@ public class SessionPersistenceMongodb implements SessionPersistence {
         }
         List<MemberEntity> memberEntities = new ArrayList<>();
         for (Member member : members) {
-            member.accept(this);
-            memberEntities.add(this.memberEntity);
+            memberEntities.add(new MemberEntityFinder(this).find(member));
         }
         sessionEntity.setMemberEntities(memberEntities);
         this.sessionDao.save(sessionEntity);
@@ -69,22 +70,38 @@ public class SessionPersistenceMongodb implements SessionPersistence {
         }
     }
 
-    @Override
-    public void visit(Package pakage) {
-        Optional<PackageEntity> packageEntity = this.packageDao.findById(pakage.getId());
-        if (packageEntity.isEmpty()) {
-            throw new PersistenceException(ErrorMessage.MEMBER_NOT_FOUND, pakage.getName());
-        }
-        this.memberEntity = packageEntity.get();
-    }
+    class MemberEntityFinder implements MemberVisitor {
 
-    @Override
-    public void visit(Class clazz) {
-        Optional<ClassEntity> classEntity = this.classDao.findById(clazz.getId());
-        if (classEntity.isEmpty()) {
-            throw new PersistenceException(ErrorMessage.MEMBER_NOT_FOUND, clazz.getName());
+        private SessionPersistenceMongodb sessionPersistence;
+        private MemberEntity memberEntity;
+
+        MemberEntityFinder(SessionPersistenceMongodb sessionPersistence) {
+            this.sessionPersistence = sessionPersistence;
         }
-        this.memberEntity = classEntity.get();
+
+        MemberEntity find(Member member) {
+            member.accept(this);
+            return this.memberEntity;
+        }
+
+        @Override
+        public void visit(Package pakage) {
+            Optional<PackageEntity> packageEntity = this.sessionPersistence.getPackageDao().findById(pakage.getId());
+            if (packageEntity.isEmpty()) {
+                throw new PersistenceException(ErrorMessage.MEMBER_NOT_FOUND, pakage.getName());
+            }
+            this.memberEntity = packageEntity.get();
+        }
+
+        @Override
+        public void visit(Class clazz) {
+            Optional<ClassEntity> classEntity = this.sessionPersistence.getClassDao().findById(clazz.getId());
+            if (classEntity.isEmpty()) {
+                throw new PersistenceException(ErrorMessage.MEMBER_NOT_FOUND, clazz.getName());
+            }
+            this.memberEntity = classEntity.get();
+        }
+
     }
 
 }
