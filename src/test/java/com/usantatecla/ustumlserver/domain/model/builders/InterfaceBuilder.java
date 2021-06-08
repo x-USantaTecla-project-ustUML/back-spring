96cc3134a43b4796be679ad9fa7c6d7ee68669fc
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2021-06-08 13:07:33 +0200

    Interface tests implemented #51

---
+++ src/test/java/com/usantatecla/ustumlserver/domain/model/builders/InterfaceBuilder.java
@@ -0,0 +1,131 @@
+package com.usantatecla.ustumlserver.domain.model.builders;
+
+import com.usantatecla.ustumlserver.domain.model.*;
+import com.usantatecla.ustumlserver.domain.model.Class;
+
+public class InterfaceBuilder extends ClassBuilder {
+
+    @Override
+    public InterfaceBuilder id(String id) {
+        super.id(id);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder name(String name) {
+        super.name(name);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder type(String name) {
+        super.type(name);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder parameter() {
+        super.parameter();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder modifiers(Modifier... modifiers) {
+        super.modifiers(modifiers);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder attributes(Attribute... attributes) {
+        super.attributes(attributes);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder methods(Method... methods) {
+        super.methods(methods);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _public() {
+        super._public();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _private() {
+        super._private();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _package() {
+        super._package();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _protected() {
+        super._protected();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _abstract() {
+        super._abstract();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _final() {
+        super._final();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder _static() {
+        super._static();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder attribute() {
+        super.attribute();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder method() {
+        super.method();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder use() {
+        super.use();
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder target(Member member) {
+        super.target(member);
+        return this;
+    }
+
+    @Override
+    public InterfaceBuilder role(String role) {
+        super.role(role);
+        return this;
+    }
+
+    @Override
+    public Interface build() {
+        return (Interface) super.build();
+    }
+
+    @Override
+    public Class createClass() {
+        return new Interface(this.name, this.modifiers, this.attributes);
+    }
+}
