2383f2c51f87cd9dfe8ea98f6da285b397bd354b
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-06-25 16:48:59 +0200

    set same site cookies

--- src/main/java/com/usantatecla/ustumlserver/configuration/WebMvcConfiguration.java
+++ src/main/java/com/usantatecla/ustumlserver/configuration/WebMvcConfiguration.java
@@ -1,5 +1,9 @@
 package com.usantatecla.ustumlserver.configuration;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +16,15 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**").allowedMethods("*")
                 .allowedOrigins("https://ust-uml-tool.web.app", "https://ust-uml-tool.firebaseapp.com/")
                 .allowCredentials(true).maxAge(3600);
+    }
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
     }
 
 }
